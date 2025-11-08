3790afbe7d06fb39953523da2fe36d7ee81d3951
Arun Kumar <81700796+ArunKr2@users.noreply.github.com>
2025-11-08 10:04:17 +0530

    Delete PlayWrightProject directory

--- PlayWrightProject/src/test/java/locators/Test1Locators.java
+++
@@ -1,8 +0,0 @@
-package locators;
-
-public class Test1Locators {
-
-    public static final String username = "#user-name";
-    public static final String password = "Password";
-    public static final String submit = "Login";
-}
--- PlayWrightProject/src/test/java/locators/Test2Locators.java
+++
@@ -1,8 +0,0 @@
-package locators;
-
-public class Test2Locators {
-
-    public static final String filter = "product-sort-container";
-    public static final String 
-
-}
